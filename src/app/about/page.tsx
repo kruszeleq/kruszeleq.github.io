c0046c22247c3d3154dd1ab50c0cdd3f3ce3a572
kruszeleq <kruszeleq@gmail.com>
2024-08-22 13:17:16 +0200

    feat: added techstack, technologies

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import Config from "../app.config";
+import { TechStackCard } from "@/components/about/techStackCard";
+import {
+  BiLogoCss3,
+  BiLogoGit,
+  BiLogoHtml5,
+  BiLogoJavascript,
+  BiLogoMongodb,
+  BiLogoNodejs,
+  BiLogoTailwindCss,
+  BiLogoTypescript,
+  BiLogoVisualStudio,
+} from "react-icons/bi";
+import { FaNpm } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
 
 const page = () => {
   return (
-    <main className="mt-[100px]">
+    <main className="mt-[150px]">
       <div className="px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] bg-[#1e1e1f] rounded-3xl text-white mx-3 mb-5 overflow-hidden">
         <header>
           <div className="text-2xl font-bold text-white mb-5 justify-center fadein-bot title-section flex items-center">
@@ -19,7 +33,7 @@ const page = () => {
               fetchPriority="high"
               width={300}
               height={300}
-              className="w-9/12 rounded-full mb-3 fadein-up border-4 border-red-400 shadow-[0_0_73px_-9px_rgba(255,112,112,.44)]"
+              className="w-5/12 md:w-8/12 rounded-full mb-3 fadein-up border-4 border-red-400 shadow-[0_0_73px_-9px_rgba(255,112,112,.44)]"
               src={Config.avatarURL}
             ></Image>
           </div>
@@ -36,9 +50,86 @@ const page = () => {
           </div>
         </section>
       </div>
-      <footer className="w-full h-full text-gray-700 font-semibold flex justify-center">
-        Więcej wkrótce...
-      </footer>
+      <div className="my-20 space-y-20">
+        <div className="m-10 h-full flex justify-center flex-col">
+          <header>
+            <div className="text-2xl font-bold text-white mb-5 justify-center fadein-bot title-section flex items-center delay-300">
+              <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>&nbsp; Tech
+              Stack &nbsp;
+              <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>
+            </div>
+          </header>
+          <div className="flex flex-wrap justify-center items-center gap-5 delay-500">
+            <TechStackCard
+              name="Typescript"
+              Icon={BiLogoTypescript}
+              advanced="Średniozaawansowany"
+            />
+            <TechStackCard
+              name="Javascript"
+              Icon={BiLogoJavascript}
+              advanced="Zaawansowany"
+            />
+            <TechStackCard
+              name="Next.js"
+              Icon={RiNextjsFill}
+              advanced="Początkujący"
+            />
+            <TechStackCard
+              name="HTML"
+              Icon={BiLogoHtml5}
+              advanced="Średniozaawansowany"
+            />
+            <TechStackCard
+              name="CSS"
+              Icon={BiLogoCss3}
+              advanced="Średniozaawansowany"
+            />
+            <TechStackCard
+              name="Tailwind"
+              Icon={BiLogoTailwindCss}
+              advanced="Średniozaawansowany"
+            />
+          </div>
+        </div>
+
+        <div className="m-10 h-full flex justify-center mt-10 flex-col">
+          <header>
+            <div className="text-2xl font-bold text-white mb-5 justify-center fadein-bot title-div flex items-center delay-300">
+              <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>&nbsp;
+              Narzędzia &nbsp;
+              <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>
+            </div>
+          </header>
+          <div className="flex flex-wrap justify-center items-center gap-5">
+            <TechStackCard
+              name="Visual Studio Code"
+              Icon={BiLogoVisualStudio}
+              advanced="Edytor kodu"
+            />
+            <TechStackCard
+              name="NPM"
+              Icon={FaNpm}
+              advanced="Menadżer pakietów"
+            />
+            <TechStackCard
+              name="MongoDB"
+              Icon={BiLogoMongodb}
+              advanced="Bazy danych"
+            />
+            <TechStackCard
+              name="Node"
+              Icon={BiLogoNodejs}
+              advanced="Środowisko wykonawcze JavaScript"
+            />
+            <TechStackCard
+              name="Git"
+              Icon={BiLogoGit}
+              advanced="Kontrola wersji"
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
---
+++ src/app/projects/nextron/page.tsx
@@ -0,0 +1,125 @@
+import Config from "@/app/app.config";
+import { TechStackCard } from "@/components/about/techStackCard";
+import Image from "next/image";
+import React from "react";
+import {
+  BiLogoDiscordAlt,
+  BiLogoMongodb,
+  BiLogoTypescript,
+  BiMusic,
+} from "react-icons/bi";
+import { FiExternalLink } from "react-icons/fi";
+import { RxSlash } from "react-icons/rx";
+
+function NextronPage() {
+  return (
+    <div className="mt-[150px]">
+      <div className="px-5 py-5 md:px-12 md:py-10 text-left border border-[#383838] bg-[#1e1e1f] rounded-3xl text-white mx-3 mb-5 overflow-hidden">
+        <div className="flex text-center items-center ">
+          <Image
+            src={Config.nextron}
+            alt="Nextron"
+            width={1}
+            height={1}
+            draggable={false}
+            className="rounded-3xl w-1/4 fadein-left hidden md:block"
+          />
+          <div className="mx-10 space-y-10 ">
+            <h1 className="text-4xl font-semibold fadein-right fadeins-1">
+              Nextron
+            </h1>
+
+            <p className="text-xl fadein-right fadeins-2">
+              Nextron jest botem muzycznym wykorzystującym Discord.js, bazę
+              danych MongoDB oraz moduł DisTube.
+              <br /> Obsługiwane platformy: Soundcloud, Spotify
+            </p>
+            <button className="border fadein-up delay-1000 border-red-400 rounded-3xl bg-red-400 bg-opacity-10 p-3 px-5 inline-flex">
+              <span>Zobacz na github</span>
+              <FiExternalLink className="ml-2 w-5 h-5" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="m-10 h-full flex justify-center mt-36 flex-col">
+        <header>
+          <div className="text-2xl font-bold text-white mb-5 justify-center fadein-bot title-div flex items-center delay-300">
+            <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>&nbsp;
+            Technologie &nbsp;
+            <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>
+          </div>
+        </header>
+        <div className="flex flex-wrap justify-center items-center gap-5">
+          <TechStackCard
+            name="Typescript"
+            Icon={BiLogoTypescript}
+            advanced="Superset Javascript"
+          />
+          <TechStackCard
+            name="MongoDB"
+            Icon={BiLogoMongodb}
+            advanced="Baza danych"
+          />
+          <TechStackCard
+            name="Discord.js"
+            Icon={BiLogoDiscordAlt}
+            advanced="Biblioteka do obsługi Discord API"
+          />
+          <TechStackCard
+            name="Distube"
+            Icon={BiMusic}
+            advanced="Obsługa kolejek, wyszukiwanie muzyki"
+          />
+        </div>
+      </div>
+      <div className="m-10 h-full flex justify-center mt-36 mb-20 flex-col">
+        <header>
+          <div className="text-2xl font-bold text-white mb-5 justify-center fadein-bot title-div flex items-center delay-300">
+            <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>&nbsp;
+            Przykładowe komendy &nbsp;
+            <div className="h-[1px] w-16 bg-red-400 md:w-48"></div>
+          </div>
+        </header>
+        <div className="flex flex-wrap justify-center items-center gap-5">
+          <TechStackCard
+            name="play"
+            Icon={RxSlash}
+            advanced="Wyszukuje i odtwarza muzykę"
+          />
+          <TechStackCard
+            name="pause"
+            Icon={RxSlash}
+            advanced="Zatrzymuje/wznawia muzykę"
+          />
+          <TechStackCard
+            name="queue"
+            Icon={RxSlash}
+            advanced="Pokazuje utwory w kolejce"
+          />
+          <TechStackCard
+            name="lyrics"
+            Icon={RxSlash}
+            advanced="Pokazuje tekst utworu"
+          />
+          <TechStackCard
+            name="stop"
+            Icon={RxSlash}
+            advanced="Zatrzymuje kolejkę, usuwa z niej utwory"
+          />
+          <TechStackCard
+            name="autoplay"
+            Icon={RxSlash}
+            advanced="Automatycznie odtwarza kolejne powiązane utwory"
+          />
+          <TechStackCard
+            name="filter"
+            Icon={RxSlash}
+            advanced="Dodanie efektów do muzyki, np nightcore"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NextronPage;
