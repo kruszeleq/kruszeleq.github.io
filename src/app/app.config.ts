2257f0a3375022171949277d34184551336aa0b0
kruszeleq <kruszeleq@gmail.com>
2024-08-24 18:15:10 +0200

    fix: changed photo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ const Config = {
 	nextron:
 		"https://cdn.discordapp.com/avatars/1081540592435200091/bf80b69d2b99da64e944a78c627a0b3e.webp?size=1024&format=webp&width=0&height=256",
 	typescript:
-		"https://media.discordapp.net/attachments/1187741918499713136/1275895018476802088/1moJeTvW97yShLB7URRj5Kg.png?ex=66c78d55&is=66c63bd5&hm=78e987085c4f5dbd295697e25e1da4b0d56c630e324a1fa5029fe0ce90791b3c&=&format=webp&quality=lossless&width=1192&height=671",
+		"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRylVrrFR3dHVnN4Kt406kJhRbTBllcZ02FIw&s",
 };
 
 export default Config;
