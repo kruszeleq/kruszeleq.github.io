a794db2ff5d3c8676bee0875839b5e164ba677fd
Krofi <54178864+kruszeleq@users.noreply.github.com>
2024-11-26 14:06:26 +0100

    fix: translated from en to pl
    fix: translated from en to pl

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
 			<h1 className="text-6xl font-bold mb-4 text-red-400">404</h1>
 			<p className="text-xl mb-8">Nie ma takiej strony!</p>
 			<Link href="/" className="inline-flex items-center border border-red-400 bg-red-400 bg-opacity-10 px-5 py-2 rounded-xl">
-				<BiHome className="w-5 h-5 mr-2" /> <span>Back to home</span>
+				<BiHome className="w-5 h-5 mr-2" /> <span>Wróć</span>
 			</Link>
 		</div>
 	);
